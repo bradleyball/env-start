975569adb03ededafdc64f310b7d4f232d4a64fc
cacheUp <bradb26@gmail.com>
2020-01-22 12:54:03 +0300

    update

--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,40 @@
 // Update with your config settings.
-require('dotenv').config();
-const pg = require('pg');
+require("dotenv").config();
+const pg = require("pg");
 pg.defaults.ssl = true;
 
 module.exports = {
-
   development: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './data/migrations'
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     },
     useNullAsDefault: true
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true
+  }
 };
